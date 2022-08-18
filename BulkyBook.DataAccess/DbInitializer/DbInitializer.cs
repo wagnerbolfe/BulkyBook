738d79b6e875164219b9177f5fcfdbcc41dff223
Wagner Bolfe <shaper@live.com>
2022-08-18 12:07:04 -0300

    adjusted appsettings

---
+++ BulkyBook.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,10 @@
+﻿namespace BulkyBook.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
